3349b1050134a6f50f63d31f6997d98087eb60e7
Andy Huang <ahuang@bbtv-8nrce.local>
2015-07-09 13:40:18 -0700

    Auto yes for java

--- cmd/install-elasticsearch.sh
+++ cmd/install-elasticsearch.sh
@@ -5,6 +5,7 @@ wget --no-check-certificate -O deb7min.tar.gz http://www.github.com/WayneBuckhan
 tar zxvf deb7min.tar.gz
 cd *deb7min*
 bash minimal.sh keepssh
+bash minimal.sh extra
 
 # Install Java 8
 echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee /etc/apt/sources.list.d/webupd8team-java.list
@@ -12,8 +13,8 @@ echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | te
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
 apt-get update
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-apt-get install oracle-java8-installer
-apt-get install oracle-java8-set-default
+apt-get install -y oracle-java8-installer
+apt-get install -y oracle-java8-set-default
 
 # Install ElasticSearch
 echo "deb http://packages.elastic.co/elasticsearch/1.6/debian stable main" | tee -a /etc/apt/sources.list.d/elasticsearch-1.6.list
